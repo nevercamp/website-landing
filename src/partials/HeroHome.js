5ef8136fe0cded57ffc1da9ff7d4826987446885
Admin <grellier.rom@gmail.com>
2022-01-25 12:56:39 +0100

    clean

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -13,7 +13,7 @@ function HeroHome() {
           <div className="w-full text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Partenaire technologique pour{"\n"}<span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-600">entreprises innovantes</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-2xl text-gray-300 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Nous vous aidons à relever vos défis techniques.</p>
+              <p className="text-2xl text-gray-300 mb-2" data-aos="zoom-y-out" data-aos-delay="150">Nous vous aidons à relever vos défis techniques.</p>
             </div>
           </div>
 
